e094c4fb192c79932cfa850b3802c7faa849ecba
Yasser <yasserfedsi34@gmail.com>
2025-08-06 14:01:55 +0200

    hosted flask backend to vercel

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,16 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
 
+@app.route("/")
+def index():
+    return render_template("index.html")
+
 @app.route("/api/hello", methods=["GET"])
-def home():
-    return jsonify({"message": "Welcome to the Flask API!"})
+def hello():
+    return jsonify({"message": "Welcome to the Flask API Production!"})
 
 
 if __name__ == "__main__":
